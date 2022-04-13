1d38c1020bfda4b8b121b7459ec28e8c4c12a5e6
AnsonWhale <48931918+AnsonWhale@users.noreply.github.com>
2022-04-13 21:54:34 +1000

    Header and test2

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 //create some buttons to navigate to test sites
-const testButton = document.createElement('button');
-const link = 'test1.html';
-
-testButton.innerHTML = 'Test 1';
-testButton.addEventListener('click', openTest );
-
-document.body.appendChild(testButton);
-
-function openTest(){
-    window.open(link)
-}
